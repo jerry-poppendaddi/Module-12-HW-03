7029f2bdf26c5f5e816cf152967a8edee766687f
Ive Alex <ive.alex.89@gmail.com>
2024-03-23 18:33:03 -0400

    Add project files.

---
+++ Module 12/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Module_12.User;
+
+namespace Module_12
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            User.User user = new User.User();
+            Console.WriteLine("Enter your login:");
+            user.Login = Console.ReadLine();
+            Console.WriteLine("Enter your name:");
+            user.Name = Console.ReadLine();
+            Console.WriteLine("Do you have premium subscription? (Y/N)");
+            var premiumResponse = Console.ReadLine();
+            if (premiumResponse == "Y")
+            {
+                Console.WriteLine("Greetings," + user.Name);
+            }
+            else if (premiumResponse == "N")
+            {
+                ShowAds();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input");
+                Console.Read();
+            }
+        }
+        static void ShowAds()
+        {
+            Console.WriteLine("Visit our free gaming website: free.games.for.a.fool.com");
+            // Остановка на 1 с
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Purchase our music subscription!");
+            // Остановка на 2 с
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Purchase our premium subscription if you don't want to see these ads.");
+            // Остановка на 3 с
+            Thread.Sleep(3000);
+        }
+     
+    } 
+}
---
+++ Module 12/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_12.User
+{
+    public class User
+    {
+        public void Userdata()
+        {
+            User user = new User();
+
+        }
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+    }
+}
